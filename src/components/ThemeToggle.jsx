d3ad155522bf3f6b1c1da71f102612e430add0f1
okpradeep <okpradeepchoudhary@gmail.com>
2025-08-22 22:31:11 +0530

    Addig portfolio project files

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Moon, Sun } from "lucide-react";
+
+export function ThemeToggle() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    if (isDarkMode) {
+      setIsDarkMode(false);
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
+    }
+  };
+
+  return (
+    <button onClick={toggleTheme}>
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-blue-900" />
+      )}
+    </button>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export function Home() {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle />
+    </div>
+  );
+}
+// theme toogle
+// background effects
+// Navbar
+// Main content
+// Footer
