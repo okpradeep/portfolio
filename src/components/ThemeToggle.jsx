262cde2fd51c57e4fabeebaac9e3eef998a26912
Pradeep Choudhary <pradeepchoudhary@Pradeeps-MacBook-Air.local>
2025-08-24 13:31:12 +0530

    Created a navbar which is responsive for mobile devices too

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,6 +1,6 @@
 import { useState , useEffect } from "react";
 import { Moon, Sun } from "lucide-react";
-import { cn } from "../lib/utils";
+import { cn } from "@/lib/utils";
 
 export function ThemeToggle() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -29,8 +29,8 @@ export function ThemeToggle() {
   };
 
   return (
-    <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration",
-      "focus:outline-hidden"
+    <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+      "focus:outlin-hidden"
     )}>
       {isDarkMode ? (
         <Sun className="h-6 w-6 text-yellow-300" />
