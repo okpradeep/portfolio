f80a99e39b6e93414f6adf31ffa2f934cf8ff0c0
Pradeep Choudhary <pradeepchoudhary@Pradeeps-MacBook-Air.local>
2025-08-24 16:20:36 +0530

    The about section and the skills sections has been added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { NavBar } from "../components/NavBar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
 
 export function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <ThemeToggle />
       <StarBackground />
-      <NavBar/>
+      <NavBar />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+      </main>
     </div>
   );
 }
